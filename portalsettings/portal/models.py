5847918209907d2b30ed7f5a943bd80c758a9c3f
NickNakker <veikitos@yandex.ru>
2023-12-05 19:05:23 +0300

    Update

--- portalsettings/portal/models.py
+++ portalsettings/portal/models.py
@@ -8,19 +8,9 @@ class News(models.Model):
     title = models.CharField('Название статьи', max_length=100, unique=True)
     text = models.TextField('Основной текст статьи')
     date = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, verbose_name='Автор', on_delete=models.CASCADE)
+    #author = models.ForeignKey(User, verbose_name='Автор', on_delete=models.CASCADE)
 
-    views = models.IntegerField('Просмотры', default=1)
-
-    # sizes = (
-    #     ('S', 'Small'),
-    #     ('M', 'Medium'),
-    #     ('L', 'Large'),
-    #     ('XL', 'X Large'),
-    # )
-
-    # shop_sizes = models.CharField(max_length=2, choices=sizes, default='S', verbose_name='Размеры')
-
+    #views = models.IntegerField('Просмотры', default=1)
 
     def __str__(self):
         return self.title
